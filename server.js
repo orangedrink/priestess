f206d17fc7771f7c862a228fa2ecfc1ca4abee53
orangedrink <dfranks@omnipress.com>
2017-10-29 11:28:10 -0500

    fix to procfile

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var StaticServer = require('static-server');
 var server = new StaticServer({
   rootPath: '.',            // required, the root of the server file tree
-  port: 8080,               // optional, defaults to a random port
   cors: '*',                // optional, defaults to undefined
   followSymlink: true,      // optional, defaults to a 404 error
 });
