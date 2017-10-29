481a43a2fe29c9d47676e74ec238b8548f30f1b4
orangedrink <dfranks@omnipress.com>
2017-10-29 11:32:39 -0500

    server on port 80

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var StaticServer = require('static-server');
 var server = new StaticServer({
   rootPath: '.',            // required, the root of the server file tree
+  port: 80,               // optional, defaults to a random port
   cors: '*',                // optional, defaults to undefined
   followSymlink: true,      // optional, defaults to a 404 error
 });
